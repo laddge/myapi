b77ed6646e49692da5c754b165b706838c2b34de
Laddge <dev.laddge@gmail.com>
2021-04-10 17:34:26 +0900

    add redirect

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from urllib.parse import urlparse
+import os
 
 app = FastAPI()
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    if 'herokuapp' in urlparse(str(request.url)).netloc:
+        domain = os.getenv('DOMAIN', 'example.com')
+        url = '{uri.scheme}://{domain}{uri.path}?{uri.query}'.format(uri=urlparse(str(request.url)), domain=domain)
+        response = RedirectResponse(url)
+    else:
+        response = await call_next(request)
+    return response
 
 
 @app.get("/")
