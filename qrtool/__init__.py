2cbc480be15fcae98243d6201d6d99986b6e8771
Laddge <dev.laddge@gmail.com>
2021-04-12 20:21:27 +0900

    change way to read qr

--- qrtool/__init__.py
+++ qrtool/__init__.py
@@ -2,9 +2,11 @@ import os
 from base64 import b64decode, b64encode
 from io import BytesIO
 
+import cv2
+import numpy as np
 import qrcode
-import qreader
 from jinja2 import Environment, FileSystemLoader
+from PIL import Image
 
 
 def make(string):
@@ -18,8 +20,17 @@ def make(string):
 def read(b64):
     data = b64decode(b64)
     buf = BytesIO(data)
+    pil_img = Image.open(buf)
+    img = np.array(pil_img, dtype=np.uint8)
+    if img.ndim == 2:
+        pass
+    elif img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    elif img.shape[2] == 4:
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
+    detector = cv2.QRCodeDetector()
     try:
-        data = qreader.read(buf)
+        data, bbox, straight_qrcode = detector.detectAndDecode(img)
     except Exception:
         string = ''
     else:
