b48ab5b2566baaffd52b336f4f66ea27b2e5a948
Laddge <dev.laddge@gmail.com>
2021-04-20 13:56:31 +0900

    remove qrtool

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
-from typing import Optional
 from urllib.parse import urlparse
 
-from fastapi import FastAPI, Form, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse, Response
 from jinja2 import Environment, FileSystemLoader
 
 import github_kusa
-import qrtool
 
 app = FastAPI()
 
@@ -36,28 +34,3 @@ async def read_root():
 @app.get("/github-kusa")
 async def read_github_kusa(user: str = ''):
     return HTMLResponse(content=github_kusa.main(user), status_code=200)
-
-
-@app.get("/qrtool")
-async def get_qrtool():
-    return HTMLResponse(content=qrtool.main(b64='iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQIHWP4DwQACfsD/Qy7W+cAAAAASUVORK5CYII='), status_code=200)
-
-
-@app.post("/qrtool")
-async def post_qrtool(string: Optional[str] = Form(''), b64: Optional[str] = Form('')):
-    if string != '':
-        res_b64 = qrtool.make(string)
-        dl = True
-        if b64 != '':
-            res_string = qrtool.read(b64)
-        else:
-            res_string = ''
-    elif b64 != '':
-        res_string = qrtool.read(b64)
-        res_b64 = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAC0lEQVQIHWP4DwQACfsD/Qy7W+cAAAAASUVORK5CYII='
-        dl = False
-    else:
-        res_string = ''
-        res_b64 = ''
-        dl = False
-    return HTMLResponse(content=qrtool.main(res_string, res_b64, dl), status_code=200)
