e9301abb1e1c6b689b0bb48a3b1ee447de692c27
Laddge <dev.laddge@gmail.com>
2021-05-23 21:38:53 +0900

    [fix] update middleware

--- main.py
+++ main.py
@@ -11,13 +11,16 @@ app = FastAPI()
 
 
 @app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
+async def middleware(request: Request, call_next):
     if request.method == "HEAD":
-        return Response()
+        response = Response()
     elif "herokuapp" in urlparse(str(request.url)).netloc:
-        domain = os.getenv("DOMAIN", "example.com")
-        url = urlparse(str(request.url))._replace(netloc=domain).geturl()
-        response = RedirectResponse(url)
+        domain = os.getenv("DOMAIN")
+        if domain:
+            url = urlparse(str(request.url))._replace(netloc=domain).geturl()
+            response = RedirectResponse(url)
+        else:
+            response = await call_next(request)
     else:
         response = await call_next(request)
     return response
