9b3a14e8106e69396d3264109495af1f0e3bc3fd
Laddge <dev.laddge@gmail.com>
2021-09-05 20:38:35 +0900

    [fix] fix error

--- questbox/__init__.py
+++ questbox/__init__.py
@@ -65,7 +65,7 @@ def post(id, lineid, text):
     draw.arc((m, ih - m - r * 2 - 1, m + r * 2, ih - m - 1),
              start=90, end=180, fill=color, width=lw)
     logo_base = PIL.Image.new('RGBA', img.size, (255, 255, 255, 0))
-    logo = PIL.Image.open('./logo.png').resize((60, 60))
+    logo = PIL.Image.open(os.path.join(os.path.dirname(__file__), 'logo.png')).resize((60, 60))
     logo_base.paste(logo, (325, ih - 90))
     img = PIL.Image.alpha_composite(img.convert('RGBA'), logo_base.convert('RGBA'))
     path = 'img/{}.png'.format(time.time())
